35d1299633714a7b70c39a2f14e938b45da83bbf
imapolaris <imapolaris@github.com>
2018-04-08 17:52:22 +0800

    some fixes

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,79 +4,104 @@ const app = getApp()
 
 Page({
   data: {
-    motto: 'Hello World',
     userInfo: {},
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo'),
     longitude: 116.404185, // 中间点坐标
     latitude: 39.91491,
-    markers: [
-      {
-        id: 0,
-        latitude: 39.91491,
-        longitude: 116.404185,
-      }
-    ],
+    markers: [],
     controls: [
-      {
-        id: 0,
-        iconPath: '/resources/icons/refresh.png',
-        position: {
-          left: 10,
-          top: 500 - 80,
-          width: 30,
-          height: 30
-        },
-        clickable: true
-      },
-      {
-      id: 1,
-      iconPath: '/resources/icons/location.png',
-      position: {
-        left: 10,
-        top: 500-30,
-        width: 30,
-        height: 30
-      },
-      clickable: true
-    }],
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+      // {
+      //   id: 0,
+      //   iconPath: '/resources/icons/refresh.png',
+      //   position: {
+      //     left: 10,
+      //     top: 80,
+      //     width: 30,
+      //     height: 30
+      //   },
+      //   clickable: true
+      // }
+    ],
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
   },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
+
   onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
+    var that = this;
+    // 定位当前位置
+    wx.getLocation({
+      success: function (res) {
+        that.setData({
+          longitude: res.longitude,
+          latitude: res.latitude,
         })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
+      },
+    })
+
+    // 请求电站数据
+    wx.request({
+      url: 'https://www.mayew.com/chargingpileservice/api/stations',
+      header: {
+        'content-type': 'application/json'
+      },
+      success: function(res) {
+        console.log('request station info succeed.');
+        if (res.statusCode == 200) {
+          console.log(res.data);
+          that.setData({
+            markers: res.data.map((item, index) =>
+              Object.assign({}, {
+                id: item.id,
+                longitude: item.longitude,
+                latitude: item.latitude,
+                callout: {
+                  content: item.name,
+                  display: 'BYCLICK',
+                  textAlign: 'center',
+                  bgColor: '#009fde',
+                  color: '#fff',
+                  padding: 5,
+                  borderRadius: 2,
+                }
+              })),
+          });
+          console.log(that.data);
         }
-      })
-    }
+      },
+      fail: function (res) {
+        console.log('request failed.')
+        console.log(res);
+      }
+    })
+
+    // if (app.globalData.userInfo) {
+    //   this.setData({
+    //     userInfo: app.globalData.userInfo,
+    //     hasUserInfo: true
+    //   })
+    // } else if (this.data.canIUse){
+    //   // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+    //   // 所以此处加入 callback 以防止这种情况
+    //   app.userInfoReadyCallback = res => {
+    //     this.setData({
+    //       userInfo: res.userInfo,
+    //       hasUserInfo: true
+    //     })
+    //   }
+    // } else {
+    //   // 在没有 open-type=getUserInfo 版本的兼容处理
+    //   wx.getUserInfo({
+    //     success: res => {
+    //       app.globalData.userInfo = res.userInfo
+    //       this.setData({
+    //         userInfo: res.userInfo,
+    //         hasUserInfo: true
+    //       })
+    //     }
+    //   })
+    // }
   },
-  getUserInfo: function(e) {
+  getUserInfo: function (e) {
     console.log(e)
     app.globalData.userInfo = e.detail.userInfo
     this.setData({
@@ -84,41 +109,79 @@ Page({
       hasUserInfo: true
     })
   },
-  onShareAppMessage: function() {
+  onShareAppMessage: function () {
     return {
-      title: '自定义标题',
+      title: 'EV堡--最好用的电动汽车充电APP',
       path: '/pages/index',
     }
   },
 
-  controltap: function(e) {
+  controltap: function (e) {
     console.log('controltap')
   },
-  markertap: function(e) {
+  markertap: function (e) {
     console.log('markertap')
   },
-  regionchange: function(e) {
+  regionchange: function (e) {
     console.log('regionchange');
   },
 
-  scantap: function(e) {
+  scantap: function (e) {
+    var that = this;
     wx.scanCode({
       onlyFromCamera: true,
       scanType: ['qrCode'],
-      success: function() {
-        console.log('scan success')
+      success: function (res) {
+        console.log('scan succeed')
+        /*var sn = res.result;
+        var userId = '49a1384a-e0ae-4e17-ad13-f3c1bd9f6234';
+        var cType = 0;
+        wx.request({
+          url: 'https://www.mayew.com/chargingpileservice/api/charging/start',
+          header: {
+            'content-type': 'application/json'
+          },
+          method: 'POST',
+          data: {
+            sn,
+            userId,
+            cType
+          },
+          success: function(res) {
+            console.log('request succeed.')
+            if (res.statusCode == 200) {
+              var result = res.data.result;
+              if (result) {
+                console.log('启动充电成功！')
+              } else {
+                console.log('启动充电失败！')
+                console.log(res.data.message);
+              }
+            }
+          },
+          fail: function(res) {
+            console.log('request failed.')
+          },
+          complete: function(res) {
+            console.log(res);
+          }
+        })
+        */
       },
-      fail: function() {
-        console.log('scan fail')
+      fail: function (res) {
+        console.log('scan failed')
       },
-      complete: function() {
-        console.log('scan complete')
+      complete: function (res) {
+        console.log('scan completed')
+        console.log(res)
       }
     })
   },
-  personaltap: function(e) {
+  personaltap: function (e) {
     wx.navigateTo({
       url: '../me/me',
     })
+
+    console.log('personal');
   }
 })
